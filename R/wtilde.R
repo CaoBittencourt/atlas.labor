5899c7193b6cceb3b3228e80479f467441aa646c
Cao Bittencourt <caobittencourt@gmail.com>
2024-09-28 17:37:22 -0300

    tests are passing

--- R/wtilde.R
+++ R/wtilde.R
@@ -28,28 +28,3 @@ wtilde <- function(h_k, T_k, ttc, w, p = Inf, agg = T){
   return(employability)
 
 }
-
-# # employability in infinitely stratified labor market
-# tildew_inf <- function(Tk_q, hk_q, ttc_q){
-#
-#   # args validation
-#   stopifnot("'Tk_q' must be numeric and defined in the unit interval." = all(is.numeric(Tk_q), Tk_q <= 1, Tk_q >= 0))
-#   stopifnot("'hk_q' must be numeric and defined in the unit interval." = all(is.numeric(hk_q), hk_q <= 1, hk_q >= 0))
-#   stopifnot("'ttc_q' must be a task duration function defined in the unit interval." = is.function(ttc_q))
-#
-#   # return employability
-#   return(hk_q[[1]] * TA(0, Tk_q[[1]], ttc_q))
-#
-# }
-#
-# tildew_inf <- function(Tk, hk, ttc){
-#
-#   # args validation
-#   stopifnot("All arguments must be the same length." = all(lengths(list(Tk, hk, ttc)) == min(lengths(list(Tk, hk, ttc)))))
-#
-#   vec_tildew_inf <- Vectorize(tildew_inf, vectorize.args = c('Tk_q', 'hk_q', 'ttc_q'))
-#
-#   # return employability
-#   return(hk_q[[1]] * TA(0, Tk_q[[1]], ttc_q))
-#
-# }
--- tests/tests.R
+++ tests/tests.R
@@ -32,13 +32,34 @@ W <- 167000000
 w <- rlnorm(873)
 w <- W * w / sum(w)
 w <- ceiling(w)
-T_k <- runif(n)
-h_k <- runif(n)
 
-T_q <- runif(n)
-h_q <- runif(n)
+T_k <- rlogis(n)
+T_k <- T_k - min(T_k)
+T_k <- T_k / max(T_k)
+
+h_k <- rlogis(n)
+h_k <- h_k - min(h_k)
+h_k <- h_k / max(h_k)
+
+T_q <- rlogis(n)
+T_q <- T_q - min(T_q)
+T_q <- T_q / max(T_q)
+
+h_q <- rlogis(n)
+h_q <- h_q - min(h_q)
+h_q <- h_q / max(h_q)
+
 u_qq <- runif(n)
 u_qk <- runif(n)
+
+ggplot2::qplot(T_k, geom = 'density')
+ggplot2::qplot(T_q, geom = 'density')
+
+ggplot2::qplot(h_k, geom = 'density')
+ggplot2::qplot(h_q, geom = 'density')
+
+ggplot2::qplot(u_qq, geom = 'density')
+ggplot2::qplot(u_qk, geom = 'density')
 
 # task duration function
 ttc <- function(l){exp(l)}
@@ -59,12 +80,15 @@ wtilde(h_k = h_k, T_k = T_k, ttc = list_ttc[[1]], w = w, agg = F)
 wtilde(
   h_k = h_k,
   T_k = T_k,
-  ttc = list_ttc,
+  # ttc = list_ttc,
+  ttc = ttc,
   w = w,
   agg = F
 ) |>
   density() |>
-  plot()
+  plot(
+    xlim = c(0,1)
+  )
 
 # competitiveness
 vstilde(h_q = h_q, T_q = T_q, u_qk = u_qk, u_qq = u_qq, ttc = ttc, w = w, agg = T)
@@ -75,3 +99,17 @@ vstilde(h_q = h_q, T_q = T_q, u_qk = u_qk, u_qq = u_qq, ttc = list_ttc[[1]], w =
 
 vstilde(h_q = h_q, T_q = T_q, u_qk = u_qk, u_qq = u_qq, ttc = list_ttc[1], w = w, agg = T)
 vstilde(h_q = h_q, T_q = T_q, u_qk = u_qk, u_qq = u_qq, ttc = list_ttc[1], w = w, agg = F)
+
+vstilde(
+  h_q = h_q,
+  T_q = T_q,
+  u_qk = u_qk,
+  u_qq = u_qq,
+  ttc = ttc,
+  w = w,
+  agg = F
+) |>
+  density() |>
+  plot(
+    xlim = c(0,1)
+  )
